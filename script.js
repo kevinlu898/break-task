f5062f0735ce647d974cb728ec97d51b8ca5888f
Kevin Lu <kevinlu2882@gmail.com>
2025-05-24 13:40:37 -0700

    adding functions to js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ name -> the character's name
 coins -> number of coins
 level -> the level your character is at
 xp -> the xp your character has towards the next level.
+tasks -> the list of tasks that have been created.
+id -> id for making new tasks
 */
 
 function resetDefault() {
@@ -23,6 +25,22 @@ function resetDefault() {
   localStorage.set("level", 0);
   localStorage.set("xp", 0);
   localStorage.set("name", "");
+  localStorage.set("tasks", []);
+  localStorage.set("id", 0);
+}
+
+function addTask(name) {
+  const listrn = localStorage.getItem("tasks");
+  localStorage.setItem("tasks", [...listrn, { name: name, id: id }]);
+  const curid = localStorage.getItem("id");
+  localStorage.setItem("id", curid + 1);
+}
+function removeTask(theid) {
+  const listrn = localStorage.getItem("tasks");
+  localStorage.setItem(
+    "tasks",
+    listrn.filter((x) => x.id != theid)
+  );
 }
 
 window.onload = () => {
