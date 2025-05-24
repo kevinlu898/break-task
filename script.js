39e0d1ac0c482fdb529d6671d5087e3988c18422
Kevin Lu <kevinlu2882@gmail.com>
2025-05-24 11:40:42 -0700

    make intro.html and edit script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+function timeToCoin(minutes) {
+  return Math.pow(1.01, minutes) * 2;
+}
+
+function timetoXP(minutes) {
+  return Math.pow(1.02, minutes) * 10;
+}
+
+function XPtoLevelUp(level) {
+  return Math.pow(2, level) * 100;
+}
+
+/*
+localstorage:
+name -> the character's name
+coins -> number of coins
+level -> the level your character is at
+xp -> the xp your character has towards the next level.
+*/
+
+function resetDefault() {
+  localStorage.set("coins", 0);
+  localStorage.set("level", 0);
+  localStorage.set("xp", 0);
+  localStorage.set("name", "");
+}
+
+function initCharacter(name) {
+  localStorage.setItem("name", name);
+}
+
+resetDefault();
+
+window.onload = () => {
+  if (!localStorage.getItem("name")) {
+    window.location.href = "/intro.html";
+  }
+};
